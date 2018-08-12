a7563805ba724837dfa9345f335e889127b64267
slowrance <steve.lowrance@gmail.com>
2018-08-11 22:59:54 -0700

    added basic styling and navigation

--- biggest_loser/controllers/home_controller.py
+++ biggest_loser/controllers/home_controller.py
@@ -1,9 +1,16 @@
 from pyramid.view import view_config
 
 
+def get_test_users():
+    return[
+        {'codename': "Cryin' Calories", 'percent': .034},
+        {'codename': "Water Horse", 'percent': .021},
+        {'codename': "Moosin' Around", 'percent': -.012},
+    ]
+
 @view_config(route_name='home', renderer="biggest_loser:templates/home/index.pt")
 def home_index(request):
-    return {'project': 'biggest_loser'}
+    return {'users': get_test_users()}
 
 
 @view_config(route_name='about', renderer="biggest_loser:templates/home/about.pt")
